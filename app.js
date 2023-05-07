a5d2dc3a993401623f2f14b9c115d905fdcfcf85
ahmad20 <juliusahmad@gmail.com>
2023-05-07 16:20:09 +0700

    app

---
+++ app.js
@@ -0,0 +1,152 @@
+const express = require('express'); //
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Konfigurasi database
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user:  process.env.DB_USER,
+  password:  process.env.DB_PASSWORD,
+  database:  process.env.DB_NAME
+});
+
+// Koneksi ke database
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Terhubung ke database');
+});
+
+// Middleware
+app.use(express.json());
+
+// Authentication middleware
+const auth = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.slice(7);
+
+  if (!token) {
+    return res.status(401).send('Tidak ada token yang disediakan');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).send('Token tidak valid');
+  }
+};
+
+// Route untuk registrasi user baru
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = {
+    name,
+    email,
+    password: hashedPassword
+  };
+
+  const sql = 'INSERT INTO users SET ?';
+
+  db.query(sql, user, (err, result) => {
+    if (err) throw err;
+    res.send('User berhasil terdaftar');
+    });
+});
+
+// Route untuk login
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = 'SELECT * FROM users WHERE email = ?';
+
+  db.query(sql, [email], (err, results) => {
+    if (err) throw err;
+    if (results.length === 0) {
+      return res.status(401).send('Email atau password salah');
+    }
+    const user = results[0];
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).send('Email atau password salah');
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, 
+      process.env.SECRET_TOKEN, 
+      { expiresIn: '1h' });
+    res.send({ token });
+  });
+});
+// Route untuk menampilkan semua post
+app.get('/posts', auth, (req, res) => {
+  const sql = 'SELECT * FROM posts';
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+// Route untuk menambah post
+app.post('/posts', auth, (req, res) => {
+  const { title, content } = req.body;
+
+  const post = {
+    title,
+    content,
+    user_id: req.user.id
+  };
+
+  const sql = 'INSERT INTO posts SET ?';
+
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    res.send('Post berhasil ditambahkan');
+  });
+});
+
+// Route untuk mengupdate post
+app.put('/posts/:id', auth, (req, res) => {
+  const { title, content } = req.body;
+  const postId = req.params.id;
+
+  const sql = 'UPDATE posts SET title = ?, content = ? WHERE id = ?';
+
+  db.query(sql, [title, content, postId], (err, result) => {
+    if (err) throw err;
+    res.send('Post berhasil diupdate');
+    });
+});
+
+// Route untuk menghapus post
+app.delete('/posts/:id', auth, (req, res) => {
+  const postId = req.params.id;
+
+  const sql = 'DELETE FROM posts WHERE id = ?';
+
+  db.query(sql, [postId], (err, result) => {
+    if (err) throw err;
+    res.send('Post berhasil dihapus');
+  });
+});
+
+
+// Menjalankan server
+app.listen(port, () => {
+  console.log(`Aplikasi berjalan di http://localhost:${port}`);
+});
+
+module.export = app;
+
+// https://documenter.getpostman.com/view/10747973/2s93eX2ZPF
---
+++ app.test.js
@@ -0,0 +1,107 @@
+const request = require("supertest");
+const expect = require("chai").expect;
+const app = require("./app");
+const mocha = require("mocha");
+
+let token;
+let baseurl = 'http://localhost:3000'
+
+describe("Test the authentication and CRUD functionality", () => {
+  // Test register new user
+  it('Should register new user', async () => {
+    const response = await request(baseurl)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ name:'newname', email: 'newemail@example.com', password: '123456' });
+    expect(response.status).to.equal(200);
+    console.log(response.text);
+  });
+  // Test authentication
+  it('Should authenticate the user and get the token', async () => {
+    const response = await request(baseurl)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ email: 'johndoe@example.com', password: '123456' });
+    expect(response.status).to.equal(200);
+    expect(response.body.token).to.not.be.an('undefined');
+    token = response.body.token;
+    console.log(response.body);
+  });
+
+  // Test create post functionality
+  it('Should create a new post', async () => {
+    const response = await request(baseurl)
+      .post('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ title: 'New Post', content: 'This is a new post.' });
+    expect(response.status).to.equal(200);
+    console.log(response.text);
+  });
+
+  // Test get all posts functionality
+  it('Should get all posts', async () => {
+    const response = await request(baseurl)
+      .get('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+    expect(response.status).to.equal(200);
+    expect(response.body.length).to.be.above(0);
+    console.log(response.body);
+  });
+
+  // Test update post functionality
+  it('Should update the latest post', async () => {
+    // Get latest posts
+    const latest = await request(baseurl)
+      .get('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+    let latest_id = latest.body.slice(0)[0].id;
+
+    const response = await request(baseurl)
+      .put(`/posts/${latest_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ title: 'Updated Post', content: 'This post has been updated.' });
+    expect(response.status).to.equal(200);
+
+    const posts = await request(baseurl)
+      .get('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+
+    console.log(posts.body);
+  });
+
+  // Test delete post functionality
+  it('Should delete the newest post', async () => {
+    // Get newest post
+    const newest = await request(baseurl)
+      .get('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+    let newest_id = newest.body.slice(-1)[0].id;
+
+    const response = await request(baseurl)
+      .delete(`/posts/${newest_id}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.status).to.equal(200);
+
+    const posts = await request(baseurl)
+      .get('/posts')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+
+    console.log(posts.body);
+  });
+});
